e18950d641a72445102ad54d3bfe441e322fc56a
Artemis <mgmahi@archlinux.localdomain>
2025-05-13 10:59:35 +0530

    Init Auth

--- traskova-app/app/layout.js
+++ traskova-app/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from '@/components/header';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${inter.className}`}>
         {/* header */}
+        <Header />
         <main className="min-h-screen">{children}</main>
         {/* footer */}
         <footer className="bg-blue-50 py-12">
